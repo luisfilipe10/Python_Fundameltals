760a56dfa54515518cb2a377cf8cfeef91e51d1f
Luis Martins <98245288+luisfilipe10@users.noreply.github.com>
2025-09-01 13:25:17 -0300

    we

---
+++ Operadores/associacao.py
@@ -0,0 +1,6 @@
+curso = "Python Fundamentos"
+saques = [1200, 302]
+frutas = ["laranja", "banana", "uva"]
+
+
+"python" in curso
---
+++ Operadores/identidade.py
@@ -0,0 +1,10 @@
+nome = "curso" 
+curso = "python"
+
+nome is curso  # True
+curso is not nome # False
+
+saldo = 1000
+limite = 200
+
+print(saldo is limite) # False
