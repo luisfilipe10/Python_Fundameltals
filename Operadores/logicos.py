5e8e0d61e9932160fc743011560e8c79e47c701a
Luis Martins <98245288+luisfilipe10@users.noreply.github.com>
2025-09-01 12:44:08 -0300

    gf

--- Operadores/logicos.py
+++ Operadores/logicos.py
@@ -1,3 +1,4 @@
+# Operadores lógicos: and, or, not
 saldo = 1000
 saque = 200
 limite = 400
@@ -11,3 +12,27 @@ if saque > saldo or saque > limite:
         print("Saque acima do limite")
 else:
     print("Saque permitido")
+
+# Negação
+
+not True  # False
+not False  # True
+
+not 1400 < 300
+
+true and true # True
+false and false # False
+true and false # False  
+false and true # False
+
+true or true # True
+false or false # False
+true or false # True
+false or true # True
+
+not true # False
+not false # True
+not (true and false) # True
+not (true or false) # False
+not (false or false)
+
